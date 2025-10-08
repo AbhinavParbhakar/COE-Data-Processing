f9fd308588a9b18f3400d31de205eb78da5d2028
Abhinav parbhakar <abhinav.parbhakar302@gmail.com>
2025-10-07 21:03:15 -0600

    Implemented all changes to fix the outliers

--- General Automations/aggregate_blackcat.py
+++ General Automations/aggregate_blackcat.py
@@ -2,7 +2,8 @@ import pandas as pd
 import os
 import tqdm
 
-ROWS_SKIPPED_BEFORE_READING_TXT_FILE = 13
+ROWS_SKIPPED_BEFORE_READING_TXT_FILE = 15
+ROWS_SKIPPED_SPECIAL_FILE = 14
 MAX_METADATA_ROW_NUM = 13
 BLACKCAT_DIRECTORY_NAME = './Associated Files/Blackcat 2025/'
 
@@ -23,19 +24,27 @@ def scrape_study(study_path)->pd.DataFrame:
     directions_set = {'EB','SB','WB','NB'}
     lane_direction_mapping = dict()
     
-    df = pd.read_csv(study_path,skiprows=ROWS_SKIPPED_BEFORE_READING_TXT_FILE)
+    # Have to check for 'new' because of ONE FILE that has a different heading arrangement
+    if 'New' in study_path or 'NEW' in study_path:
+        df = pd.read_csv(study_path,skiprows=ROWS_SKIPPED_BEFORE_READING_TXT_FILE)
+        date_col_name = 'Date'
+        lane_column_name = ' Channel'
+        df[date_col_name] = df.index
+    else:
+        df = pd.read_csv(study_path,skiprows=ROWS_SKIPPED_SPECIAL_FILE,sep=' ')
+        date_col_name = 'Date'
+        lane_column_name = 'Lane'
     
-    date_col_name = 'Date'
     direction_col_name = 'Direction'
-    lane_column_name = ' Channel'
-    df[date_col_name] = df.index
+    
+
     
     with open(study_path,mode='rt') as f:
         next(f) # Skip first line, do not need to read the file name
         for i in range(MAX_METADATA_ROW_NUM):
             metadata = f.readline().rstrip()
             information_split =  metadata.split(': ')
-            assert information_split.__len__() == 2, "Metadata not in the format <Key>:<Value>"
+            assert information_split.__len__() == 2, f"Metadata not in the format <Key>:<Value> for {study_path}"
             key = information_split[0]
             value = information_split[1]
             
@@ -43,7 +52,12 @@ def scrape_study(study_path)->pd.DataFrame:
                 # In this case, the value will be the lane number that is referenced in the data columns
                 # As such, store the value as the key, and the key (direction) as the mapping
                 # So that we can revert later
-                lane_direction_mapping[f' Lane {value}'] = key
+                
+                # SAME THING, checking for new smh
+                if 'New' in study_path or 'NEW' in study_path:
+                    lane_direction_mapping[f' Lane {value}'] = key
+                else:
+                    lane_direction_mapping[value] = key
             else:
                 keys_list.append(key)
                 values_list.append(value)
@@ -53,10 +67,14 @@ def scrape_study(study_path)->pd.DataFrame:
     df[direction_col_name] = df[lane_column_name].map(lane_direction_mapping)
     
     # Group data based on Date and Direction
-    aggregate_count_df = df.groupby([date_col_name,direction_col_name],as_index=False)[[lane_column_name]].count()
-    aggregate_count_df = aggregate_count_df.rename({lane_column_name:'Traffic Count'},axis=1)
+    try:
+        aggregate_count_df = df.groupby([date_col_name,direction_col_name],as_index=False)[[lane_column_name]].count()
+        aggregate_count_df = aggregate_count_df.rename({lane_column_name:'Traffic Count'},axis=1)
+    except KeyError as e:
+        print(f'File {study_path} caused an error')
+        raise e
     
-    for i,key in keys_list:
+    for i,key in enumerate(keys_list):
         aggregate_count_df[key] = pd.Series([values_list[i]] * aggregate_count_df.shape[0])
 
     return aggregate_count_df
